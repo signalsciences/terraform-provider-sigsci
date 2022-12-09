5be588c9351574382a2c41277e59c4209ac3bee3
Shawn Smith <shawnps@fastly.com>
2022-12-09 15:00:44 +0900

    add ability to override API URL with SIGSCI_URL environment variable

--- provider/config.go
+++ provider/config.go
@@ -2,11 +2,13 @@ package provider
 
 import (
 	"fmt"
+
 	"github.com/signalsciences/go-sigsci"
 )
 
 //Config struct for email and password
 type Config struct {
+	URL      string
 	Email    string
 	Password string
 	APIToken string
@@ -14,13 +16,19 @@ type Config struct {
 
 //Client returns a signal science client
 func (c *Config) Client() (interface{}, error) {
+	if c.URL != "" {
+		sigsci.SetAPIUrl(c.URL)
+	}
+
 	if c.Email == "" {
 		return nil, fmt.Errorf("email cannot be empty")
 	}
+
 	if c.APIToken != "" {
 		return sigsci.NewTokenClient(c.Email, c.APIToken), nil
 	} else if c.Password != "" {
 		return sigsci.NewClient(c.Email, c.Password)
 	}
+
 	return nil, fmt.Errorf("you must provide either password or api_token")
 }
