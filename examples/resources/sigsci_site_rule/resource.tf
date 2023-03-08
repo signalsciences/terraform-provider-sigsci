4d638691ca1bd4261fd475ddfc763036dce8ffe7
Shawn Smith <shawnps@fastly.com>
2023-03-08 13:08:31 +0900

    add examples for all 3 site rule types (request, rate limit, signal exclusion)

--- examples/resources/sigsci_site_rule/resource.tf
+++ examples/resources/sigsci_site_rule/resource.tf
@@ -1,10 +1,9 @@
-resource "sigsci_site_rule" "test" {
+resource "sigsci_site_rule" "test-request-rule" {
   site_short_name = sigsci_site.my-site.short_name
-  type            = "signal"
-  group_operator  = "any"
+  type            = "request"
+  group_operator  = "all"
   enabled         = true
-  reason          = "Example site rule update"
-  signal          = "SQLI"
+  reason          = "Example request site rule"
   requestlogging  = "sampled"
   expiration      = ""
 
@@ -16,17 +15,74 @@ resource "sigsci_site_rule" "test" {
   }
 
   conditions {
+    type           = "multival"
+    field          = "requestHeader"
+    operator       = "exists"
+    group_operator = "all"
+
+    conditions {
+      type     = "single"
+      field    = "name"
+      operator = "equals"
+      value    = "Content-Type"
+    }
+
+    conditions {
+      type     = "single"
+      field    = "valueString"
+      operator = "equals"
+      value    = "application/json"
+    }
+  }
+
+  actions {
+    type = "block"
+  }
+}
+
+resource "sigsci_site_rule" "test-ratelimit-rule" {
+  site_short_name = sigsci_site.my-site.short_name
+  type            = "rateLimit"
+  group_operator  = "all"
+  enabled         = true
+  reason          = "Example rate limit rule"
+  signal          = "site.signal_id"
+  expiration      = ""
+
+  conditions {
     type     = "single"
     field    = "ip"
     operator = "equals"
     value    = "1.2.3.5"
-    conditions {
-      type           = "multival"
-      field          = "ip"
-      operator       = "equals"
-      group_operator = "all"
-      value          = "1.2.3.8"
-    }
+  }
+
+  rate_limit = {
+    threshold = 6
+    interval  = 10
+    duration  = 300
+  }
+
+  actions {
+    type   = "logRequest"
+    signal = "site.signal_id"
+  }
+}
+
+
+resource "sigsci_site_rule" "test-signal-exclusion" {
+  site_short_name = sigsci_site.my-site.short_name
+  type            = "signal"
+  group_operator  = "all"
+  enabled         = true
+  reason          = "Example signal exclusion site rule"
+  signal          = "SQLI"
+  expiration      = ""
+
+  conditions {
+    type     = "single"
+    field    = "ip"
+    operator = "equals"
+    value    = "1.2.3.6"
   }
 
   actions {
