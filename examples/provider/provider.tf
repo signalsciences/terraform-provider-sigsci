472e410430839b99ed4e3029ef47180a4cc9409f
Shawn Smith <shawnpsmith@gmail.com>
2023-03-10 12:33:35 +0900

    auto-generate docs/index.md including provider example (#127)
    * auto-generate docs/index.md including provider example
    
    * mark corp and email as required attributes in schema

---
+++ examples/provider/provider.tf
@@ -0,0 +1,7 @@
+provider "sigsci" {
+  corp  = "" // Required. may also provide via env variable SIGSCI_CORP
+  email = "" // Required. may also provide via env variable SIGSCI_EMAIL
+  //  auth_token = "" // May also provide via env variable SIGSCI_TOKEN
+  //  password = ""   // May also provide via env variable SIGSCI_PASSWORD
+  //  fastly_key = "" // May also provide via env variable FASTLY_KEY. Required for Edge Deployments functionality.
+}
