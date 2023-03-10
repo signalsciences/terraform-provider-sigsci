7c5dd6233a23c213dcaa71be091206dcee845e7a
Shawn Smith <shawnps@fastly.com>
2023-03-10 11:38:45 +0900

    uncomment required provider attributes in provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,6 +1,6 @@
 provider "sigsci" {
-  //  corp = ""       // Required. may also provide via env variable SIGSCI_CORP
-  //  email = ""      // Required. may also provide via env variable SIGSCI_EMAIL
+  corp  = "" // Required. may also provide via env variable SIGSCI_CORP
+  email = "" // Required. may also provide via env variable SIGSCI_EMAIL
   //  auth_token = "" // May also provide via env variable SIGSCI_TOKEN
   //  password = ""   // May also provide via env variable SIGSCI_PASSWORD
   //  fastly_key = "" // May also provide via env variable FASTLY_KEY. Required for Edge Deployments functionality.
