0288f6aaa1f69053860ff4c3921dc6ca8b30251a
vvuksan-fastly <67602984+vvuksan-fastly@users.noreply.github.com>
2023-03-23 21:22:10 -0400

    Add support for JSON arguments to EdgeDeployment (#143)
    * Add support for JSON arguments to EdgeDeployment
    
    * Remove defaults to it defaults to API defaults
    
    * updateEdgeDeploymentBackends needs to be rethought as terraform may not be the best
        place to trigger that from unless we are able to exploit dependency from a Fastly service
    
    * Bump to go-sigsci to v0.1.14
    
    * Revert "Remove defaults to it defaults to API defaults"
    
    This reverts commit c304ffea937029e93aba5958ee93149db0ea4012.
    
    ---------
    
    Co-authored-by: Vladimir Vuksan <vladimir@fastly.com>

--- provider/resource_edge_deployment_service.go
+++ provider/resource_edge_deployment_service.go
@@ -2,13 +2,14 @@ package provider
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/signalsciences/go-sigsci"
 )
 
 func resourceEdgeDeploymentService() *schema.Resource {
 	return &schema.Resource{
 		Create:   createOrUpdateEdgeDeploymentService,
 		Read:     readEdgeDeploymentService,
-		Update:   updateEdgeDeploymentBackends,
+		Update:   createOrUpdateEdgeDeploymentService,
 		Delete:   detachEdgeDeploymentService,
 		Importer: &schema.ResourceImporter{},
 		Schema: map[string]*schema.Schema{
@@ -23,6 +24,20 @@ func resourceEdgeDeploymentService() *schema.Resource {
 				Description: "Fastly service ID",
 				Required:    true,
 			},
+
+			"activate_version": {
+				Type:        schema.TypeBool,
+				Description: "activate Fastly service version after clone. Possible values are true or false",
+				Optional:    true,
+				Default:     true,
+			},
+
+			"percent_enabled": {
+				Type:        schema.TypeInt,
+				Description: "percentage of traffic to send to NGWAF@Edge. Possible values are integers values 0 to 100",
+				Optional:    true,
+				Default:     0,
+			},
 		},
 	}
 }
@@ -32,17 +47,15 @@ func createOrUpdateEdgeDeploymentService(d *schema.ResourceData, m interface{}) 
 
 	d.SetId(d.Get("fastly_sid").(string))
 
-	return pm.Client.CreateOrUpdateEdgeDeploymentService(pm.Corp, d.Get("site_short_name").(string), d.Get("fastly_sid").(string))
+	activateVersion := d.Get("activate_version").(bool)
+	return pm.Client.CreateOrUpdateEdgeDeploymentService(pm.Corp, d.Get("site_short_name").(string), d.Get("fastly_sid").(string), sigsci.CreateOrUpdateEdgeDeploymentServiceBody{
+		ActivateVersion:         &activateVersion,
+		PercentEnabled:          d.Get("percent_enabled").(int),
+	})
 }
 
 func readEdgeDeploymentService(d *schema.ResourceData, m interface{}) error {
 	return nil
-}
-
-func updateEdgeDeploymentBackends(d *schema.ResourceData, m interface{}) error {
-	pm := m.(providerMetadata)
-
-	return pm.Client.CreateOrUpdateEdgeDeploymentService(pm.Corp, d.Get("site_short_name").(string), d.Get("fastly_sid").(string))
 }
 
 func detachEdgeDeploymentService(d *schema.ResourceData, m interface{}) error {
