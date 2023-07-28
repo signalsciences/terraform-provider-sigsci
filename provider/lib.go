c80af843a6f991bdfd3bbbd6869196da22754733
Zsolt Janka <zsoltj@gmail.com>
2023-07-28 15:42:52 -0400

    added missing ja3Fingerprint conditions.field value that is now available on the web interface

--- provider/lib.go
+++ provider/lib.go
@@ -526,7 +526,7 @@ var siteImporter = schema.ResourceImporter{
 var KnownSingleConditionFields = []string{
 	"scheme", "method", "path", "useragent", "domain", "ip", "responseCode", "agentname",
 	"paramname", "paramvalue", "country", "name", "valueString", "valueIp", "signalType",
-	"value",
+	"value","ja3Fingerprint",
 }
 
 var KnownMultivalConditionFields = []string{
