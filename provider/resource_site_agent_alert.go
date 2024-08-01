732ad4d844d279d6686dd94fe694b797a3e47c5b
Brooks Cunningham <Brooks.Cunningham@gmail.com>
2024-08-01 09:34:13 -0500

    Update resource_site_agent_alert.go

--- provider/resource_site_agent_alert.go
+++ provider/resource_site_agent_alert.go
@@ -4,14 +4,15 @@ import (
 	"errors"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/signalsciences/go-sigsci"
 )
 
 func resourceSiteAgentAlert() *schema.Resource {
 	return &schema.Resource{
-		Create:   resourceSiteAlertCreate,
-		Update:   resourceSiteAlertUpdate,
-		Read:     resourceSiteAlertRead,
-		Delete:   resourceSiteAlertDelete,
+		Create:   resourceSiteAgentAlertCreate,
+		Update:   resourceSiteAgentAlertUpdate,
+		Read:     resourceSiteAgentAlertRead,
+		Delete:   resourceSiteAgentAlertDelete,
 		Importer: &siteImporter,
 		Schema: map[string]*schema.Schema{
 			"site_short_name": {
@@ -32,7 +33,7 @@ func resourceSiteAgentAlert() *schema.Resource {
 			},
 			"interval": {
 				Type:        schema.TypeInt,
-				Description: "Integer value for interval. Must be 5, 10 or 60.",
+				Description: "Integer value for interval. Must be 5, 10, or 60.",
 				Required:    true,
 				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
 					if existsInInt(val.(int), 5, 10, 60) {
@@ -80,4 +81,112 @@ func resourceSiteAgentAlert() *schema.Resource {
 			},
 		},
 	}
+}
+
+func resourceSiteAgentAlertCreate(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	alert, err := sc.CreateCustomAlert(pm.Corp, d.Get("site_short_name").(string), sigsci.CustomAlertBody{
+		TagName:              d.Get("tag_name").(string),
+		LongName:             d.Get("long_name").(string),
+		Interval:             d.Get("interval").(int),
+		Threshold:            d.Get("threshold").(int),
+		Enabled:              d.Get("enabled").(bool),
+		Action:               d.Get("action").(string),
+		SkipNotifications:    d.Get("skip_notifications").(bool),
+		BlockDurationSeconds: d.Get("block_duration_seconds").(int),
+	})
+
+	if err != nil {
+		return err
+	}
+	d.SetId(alert.ID)
+	return resourceSiteAgentAlertRead(d, m)
+}
+
+func resourceSiteAgentAlertRead(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	alert, err := sc.GetCustomAlert(pm.Corp, d.Get("site_short_name").(string), d.Id())
+	if err != nil {
+		d.SetId("")
+		return nil
+	}
+
+	d.SetId(alert.ID)
+	err = d.Set("site_short_name", d.Get("site_short_name").(string))
+	if err != nil {
+		return err
+	}
+	err = d.Set("tag_name", alert.TagName)
+	if err != nil {
+		return err
+	}
+	err = d.Set("long_name", alert.LongName)
+	if err != nil {
+		return err
+	}
+	err = d.Set("interval", alert.Interval)
+	if err != nil {
+		return err
+	}
+	err = d.Set("threshold", alert.Threshold)
+	if err != nil {
+		return err
+	}
+	err = d.Set("enabled", alert.Enabled)
+	if err != nil {
+		return err
+	}
+	err = d.Set("action", alert.Action)
+	if err != nil {
+		return err
+	}
+	err = d.Set("skip_notifications", alert.SkipNotifications)
+	if err != nil {
+		return err
+	}
+	err = d.Set("block_duration_seconds", alert.BlockDurationSeconds)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceSiteAgentAlertUpdate(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	alert, err := sc.UpdateCustomAlert(pm.Corp, d.Get("site_short_name").(string), d.Id(), sigsci.CustomAlertBody{
+		TagName:              d.Get("tag_name").(string),
+		LongName:             d.Get("long_name").(string),
+		Interval:             d.Get("interval").(int),
+		Threshold:            d.Get("threshold").(int),
+		Enabled:              d.Get("enabled").(bool),
+		Action:               d.Get("action").(string),
+		SkipNotifications:    d.Get("skip_notifications").(bool),
+		BlockDurationSeconds: d.Get("block_duration_seconds").(int),
+	})
+	if err != nil {
+		d.SetId("")
+		return err
+	}
+
+	d.SetId(alert.ID)
+	return resourceSiteAgentAlertRead(d, m)
+}
+
+func resourceSiteAgentAlertDelete(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	err := sc.DeleteCustomAlert(pm.Corp, d.Get("site_short_name").(string), d.Id())
+	if err != nil {
+		return err
+	}
+	d.SetId("")
+	return nil
 }
