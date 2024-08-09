704f538208bce188f2d5f9bebe732853f54b9ff3
Brooks Cunningham <bcunningham@fastly.com>
2024-08-08 19:57:02 -0500

    Agent alerts fix1 (#229)
    * Updating rate limiting example
    
    * Update resource_site_agent_alert.go
    
    * Bug fix for site agent alerts
    
    * Bug fix for site agent alerts
    * Up'ed the sigsci SDK version to 0.1.21
    
    * Using sigsci go SDK version 0.1.22

--- provider/resource_site_agent_alert.go
+++ provider/resource_site_agent_alert.go
@@ -4,14 +4,15 @@ import (
 	"errors"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/signalsciences/go-sigsci"
 )
 
 func resourceSiteAgentAlert() *schema.Resource {
 	return &schema.Resource{
-		Create:   resourceSiteAlertCreate,
-		Update:   resourceSiteAlertUpdate,
-		Read:     resourceSiteAlertRead,
-		Delete:   resourceSiteAlertDelete,
+		Create:   resourceSiteAgentAlertCreate,
+		Update:   resourceSiteAgentAlertUpdate,
+		Read:     resourceSiteAgentAlertRead,
+		Delete:   resourceSiteAgentAlertDelete,
 		Importer: &siteImporter,
 		Schema: map[string]*schema.Schema{
 			"site_short_name": {
@@ -32,7 +33,7 @@ func resourceSiteAgentAlert() *schema.Resource {
 			},
 			"interval": {
 				Type:        schema.TypeInt,
-				Description: "Integer value for interval. Must be 5, 10 or 60.",
+				Description: "Integer value for interval. Must be 5, 10, or 60.",
 				Required:    true,
 				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
 					if existsInInt(val.(int), 5, 10, 60) {
@@ -43,13 +44,13 @@ func resourceSiteAgentAlert() *schema.Resource {
 			},
 			"threshold": {
 				Type:        schema.TypeInt,
-				Description: "The number of occurrences of the tag in the interval needed to trigger the alert. Min 0, Max 10000",
+				Description: "The number of occurrences of the tag in the interval needed to trigger the alert. Min 0, Max 3600000",
 				Required:    true,
 				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
-					if existsInRange(val.(int), 0, 10000) {
+					if existsInRange(val.(int), 0, 3600000) {
 						return nil, nil
 					}
-					return nil, []error{errors.New("threshold must be between 0 and 10000")}
+					return nil, []error{errors.New("threshold must be between 0 and 3600000")}
 				},
 			},
 			"enabled": {
@@ -78,6 +79,148 @@ func resourceSiteAgentAlert() *schema.Resource {
 				Description: "The number of seconds this alert is active.",
 				Optional:    true,
 			},
+			"field_name": {
+				Type:        schema.TypeString,
+				Description: "Field_name for agent alert.",
+				Required:    true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					if existsInString(val.(string), "siteMetric") {
+						return nil, nil
+					}
+					return nil, []error{errors.New("action must be 'siteMetric'")}
+				},
+			},
+			"operator": {
+				Type:        schema.TypeString,
+				Description: "Operator for agent alert.",
+				Required:    true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					if existsInString(val.(string), "notEqualTo", "equalTo", "lessThan", "greaterThan") {
+						return nil, nil
+					}
+					return nil, []error{errors.New("action must be 'notEqualTo', 'equalTo', 'lessThan', 'greaterThan'")}
+				},
+			},
 		},
 	}
+}
+
+func resourceSiteAgentAlertCreate(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	alert, err := sc.CreateCustomAlert(pm.Corp, d.Get("site_short_name").(string), sigsci.CustomAlertBody{
+		TagName:              d.Get("tag_name").(string),
+		LongName:             d.Get("long_name").(string),
+		Interval:             d.Get("interval").(int),
+		Threshold:            d.Get("threshold").(int),
+		Enabled:              d.Get("enabled").(bool),
+		Action:               d.Get("action").(string),
+		SkipNotifications:    d.Get("skip_notifications").(bool),
+		BlockDurationSeconds: d.Get("block_duration_seconds").(int),
+		FieldName:            d.Get("field_name").(string),
+		Operator:             d.Get("operator").(string),
+	})
+
+	if err != nil {
+		return err
+	}
+	d.SetId(alert.ID)
+	return resourceSiteAgentAlertRead(d, m)
+}
+
+func resourceSiteAgentAlertRead(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	alert, err := sc.GetCustomAlert(pm.Corp, d.Get("site_short_name").(string), d.Id())
+	if err != nil {
+		d.SetId("")
+		return nil
+	}
+
+	d.SetId(alert.ID)
+	err = d.Set("site_short_name", d.Get("site_short_name").(string))
+	if err != nil {
+		return err
+	}
+	err = d.Set("tag_name", alert.TagName)
+	if err != nil {
+		return err
+	}
+	err = d.Set("long_name", alert.LongName)
+	if err != nil {
+		return err
+	}
+	err = d.Set("interval", alert.Interval)
+	if err != nil {
+		return err
+	}
+	err = d.Set("threshold", alert.Threshold)
+	if err != nil {
+		return err
+	}
+	err = d.Set("enabled", alert.Enabled)
+	if err != nil {
+		return err
+	}
+	err = d.Set("action", alert.Action)
+	if err != nil {
+		return err
+	}
+	err = d.Set("skip_notifications", alert.SkipNotifications)
+	if err != nil {
+		return err
+	}
+	err = d.Set("block_duration_seconds", alert.BlockDurationSeconds)
+	if err != nil {
+		return err
+	}
+	err = d.Set("field_name", alert.FieldName)
+	if err != nil {
+		return err
+	}
+	err = d.Set("operator", alert.Operator)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceSiteAgentAlertUpdate(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	alert, err := sc.UpdateCustomAlert(pm.Corp, d.Get("site_short_name").(string), d.Id(), sigsci.CustomAlertBody{
+		TagName:              d.Get("tag_name").(string),
+		LongName:             d.Get("long_name").(string),
+		Interval:             d.Get("interval").(int),
+		Threshold:            d.Get("threshold").(int),
+		Enabled:              d.Get("enabled").(bool),
+		Action:               d.Get("action").(string),
+		SkipNotifications:    d.Get("skip_notifications").(bool),
+		BlockDurationSeconds: d.Get("block_duration_seconds").(int),
+		FieldName:            d.Get("field_name").(string),
+		Operator:             d.Get("operator").(string),
+	})
+	if err != nil {
+		d.SetId("")
+		return err
+	}
+
+	d.SetId(alert.ID)
+	return resourceSiteAgentAlertRead(d, m)
+}
+
+func resourceSiteAgentAlertDelete(d *schema.ResourceData, m interface{}) error {
+	pm := m.(providerMetadata)
+	sc := pm.Client
+
+	err := sc.DeleteCustomAlert(pm.Corp, d.Get("site_short_name").(string), d.Id())
+	if err != nil {
+		return err
+	}
+	d.SetId("")
+	return nil
 }
